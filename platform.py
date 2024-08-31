84adb46e42ae2cd576c65400672a1fd77a810a6d
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2024-08-31 18:07:02 +0200

    Update platform.py

--- platform.py
+++ platform.py
@@ -26,7 +26,7 @@ python_exe = get_pythonexe_path()
 IDF_TOOLS_PATH_DEFAULT = os.path.join(os.path.expanduser("~"), ".espressif")
 IDF_TOOLS = os.path.join(ProjectConfig.get_instance().get("platformio", "packages_dir"), "tl-install", "tools", "idf_tools.py")
 print("idf tools path:", IDF_TOOLS)
-IDF_TOOLS_STRG = str(IDF_TOOLS) + "install"
+IDF_TOOLS_STRG = str(IDF_TOOLS) + " install"
 print("idf tools strg path:", IDF_TOOLS_STRG)
 IDF_TOOLS_CMD = (
     python_exe,
