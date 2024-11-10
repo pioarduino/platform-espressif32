224ea383854caa6d7eda9b3aadcd311a85f50c68
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2024-11-10 20:16:04 +0100

    Update espidf.py

--- builder/frameworks/espidf.py
+++ builder/frameworks/espidf.py
@@ -236,10 +236,6 @@ def HandleArduinoIDFsettings(env):
             response = request.urlretrieve(sdkconfig_defconfig_url, defconfig_mcu)
             with open(defconfig_mcu, 'r') as f1, open(sdkconfig_mcu, 'a') as f2:
                 f2.write(f1.read())
-            with open(sdkconfig_src, 'r') as file:
-                file_lines = file.readlines()
-                for line in file_lines:
-                    print(line.strip())
 
         def get_flag(line):
             if line.startswith("#") and "is not set" in line:
@@ -2046,8 +2042,8 @@ if "arduino" in env.get("PIOFRAMEWORK") and "espidf" not in env.get("PIOFRAMEWOR
         for folder in src:
             files = [join(folder,x) for x in os.listdir(folder)]
             for file in files:
-                print("new lib file to copy:", file)
                 if file.strip().endswith(".a"):
+                    print("new lib file to copy:", file)
                     shutil.copyfile(file,join(lib_dst,file.split(os.path.sep)[-1]))
 
         shutil.move(join(lib_dst,"libspi_flash.a"),join(mem_var,"libspi_flash.a"))
