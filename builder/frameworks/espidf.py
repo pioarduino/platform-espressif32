6d2e6fb5722cf7c34747e132f69b33b3e0176945
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2024-11-10 18:12:30 +0100

    Update espidf.py

--- builder/frameworks/espidf.py
+++ builder/frameworks/espidf.py
@@ -235,9 +235,9 @@ def HandleArduinoIDFsettings(env):
             response = request.urlretrieve(sdkconfig_defconfig_url, defconfig_mcu)
             with open(defconfig_mcu, 'r') as f1, open(sdkconfig_src, 'w') as f2:
                 f2.write(f1.read())
-	    with open(sdkconfig_src, 'r') as file:
-		file_lines = file.readlines()
-		for line in file_lines:
+            with open(sdkconfig_src, 'r') as file:
+                file_lines = file.readlines()
+                for line in file_lines:
                     print(line.strip())
 
         def get_flag(line):
