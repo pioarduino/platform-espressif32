a427675e90fba4fbe189f63471bde2adb9ca60e3
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2024-11-10 17:57:59 +0100

    Update espidf.py

--- builder/frameworks/espidf.py
+++ builder/frameworks/espidf.py
@@ -234,11 +234,11 @@ def HandleArduinoIDFsettings(env):
             defconfig_mcu = join(ARDUINO_FRMWRK_LIB_DIR,mcu,"defconfig." + mcu)
             response = request.urlretrieve(sdkconfig_defconfig_url, defconfig_mcu)
             with open(defconfig_mcu, 'r') as f1, open(sdkconfig_src, 'w') as f2:
-		    f2.write(f1.read())
+		f2.write(f1.read())
 	    with open(sdkconfig_src, 'r') as file:
-		    file_lines = file.readlines()
-		    for line in file_lines:
-			    print(line.strip())
+		file_lines = file.readlines()
+		for line in file_lines:
+                    print(line.strip())
 
         def get_flag(line):
             if line.startswith("#") and "is not set" in line:
