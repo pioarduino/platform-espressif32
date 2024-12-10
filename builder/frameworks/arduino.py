f2a618c8ce3906232d849ceeeb76d8a58a841153
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2024-12-10 18:34:47 +0100

    prep for new script "pioarduino-build.py"

--- builder/frameworks/arduino.py
+++ builder/frameworks/arduino.py
@@ -192,4 +192,9 @@ if flag_custom_sdkconfig == True and flag_any_custom_sdkconfig == False:
     call_compile_libs()
 
 if "arduino" in env.subst("$PIOFRAMEWORK") and "espidf" not in env.subst("$PIOFRAMEWORK") and env.subst("$ARDUINO_LIB_COMPILE_FLAG") in ("Inactive", "True"):
-    SConscript(join(platform.get_package_dir("framework-arduinoespressif32"), "tools", "platformio-build.py"))
+    if os.path.exists(join(platform.get_package_dir(
+            "framework-arduinoespressif32"), "tools", "platformio-build.py")):
+        PIO_BUILD = "platformio-build.py"
+    else:
+        PIO_BUILD = "pioarduino-build.py"
+    SConscript(join(platform.get_package_dir("framework-arduinoespressif32"), "tools", PIO_BUILD))
