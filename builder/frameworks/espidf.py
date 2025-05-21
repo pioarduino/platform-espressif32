00fd9712b41d379a677817f24154bbe4af8a453d
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2025-05-21 10:13:11 +0200

    Update espidf.py

--- builder/frameworks/espidf.py
+++ builder/frameworks/espidf.py
@@ -83,7 +83,8 @@ def install_standard_python_deps():
     deps = {
         "wheel": ">=0.35.1",
         "rich-click": ">=1.8.6",
-        "PyYAML": ">=6.0.2"
+        "PyYAML": ">=6.0.2",
+        "esp-idf-size": ">=1.6.1"
     }
 
     installed_packages = _get_installed_standard_pip_packages()
