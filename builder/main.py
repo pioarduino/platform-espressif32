15acc41c38de355bff19334fd72db5249ac4d6bc
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2025-05-21 11:23:55 +0200

    try workaround to silence scons command output

--- builder/main.py
+++ builder/main.py
@@ -387,7 +387,8 @@ if "nobuild" in COMMAND_LINE_TARGETS:
         target_firm = join("$BUILD_DIR", "${PROGNAME}.bin")
 else:
     target_elf = env.BuildProgram()
-    silent_action = Action(firmware_metrics, cmdstr='')
+    silent_action = Action(firmware_metrics)
+    silent_action.strfunction = lambda target, source, env: '' # hack to silence scons command output
     env.AddPostAction(target_elf, silent_action)
     if set(["buildfs", "uploadfs", "uploadfsota"]) & set(COMMAND_LINE_TARGETS):
         target_firm = env.DataToBin(
