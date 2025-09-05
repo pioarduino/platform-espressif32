b83440e98859d139f167fc69ae32de5eed8e063f
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2025-09-05 15:00:36 +0200

    Refactor Arduino libraries path assignment

--- builder/frameworks/component_manager.py
+++ builder/frameworks/component_manager.py
@@ -52,9 +52,8 @@ class ComponentManagerConfig:
         # Get Arduino libraries installation directory
         ald = self.platform.get_package_dir("framework-arduinoespressif32-libs")
         # Get MCU-specific Arduino libraries directory
-        self.arduino_libs_mcu = (
-            str(Path(ald) / self.mcu) if ald else ""
-        )
+        self.arduino_libs_mcu_path = (Path(ald) / self.mcu)
+        self.arduino_libs_mcu = str(self.arduino_libs_mcu_path)
 
 
 class ComponentLogger:
