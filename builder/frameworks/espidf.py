c46cecb0bfaa636a39b44d4d1bb38eaa295a2846
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2026-03-15 15:30:56 +0100

    Update dependencies for idf-component-manager and esp-idf-kconfig

--- builder/frameworks/espidf.py
+++ builder/frameworks/espidf.py
@@ -2113,8 +2113,8 @@ def install_python_deps():
         # https://github.com/platformio/platform-espressif32/issues/635
         "cryptography": "~=44.0.0",
         "pyparsing": ">=3.1.0,<4",
-        "idf-component-manager": "~=2.4.6",
-        "esp-idf-kconfig": "~=2.5.0"
+        "idf-component-manager": "~=2.4",
+        "esp-idf-kconfig": "~=3.3.0"
     }
 
     if sys_platform.system() == "Darwin" and "arm" in sys_platform.machine().lower():
