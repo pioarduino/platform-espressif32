23ba1639ad45bf0521bea7019195d0479ee57516
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2026-03-26 13:03:06 +0100

    Update package versions for idf-component-manager and esp-idf-kconfig
    use latest esp-idf-kconfig since previous 3.x versions have bugs in creating correct sdkconfig entrys

--- builder/frameworks/espidf.py
+++ builder/frameworks/espidf.py
@@ -2110,8 +2110,8 @@ def install_python_deps():
         # https://github.com/platformio/platform-espressif32/issues/635
         "cryptography": "~=44.0.0",
         "pyparsing": ">=3.1.0,<4",
-        "idf-component-manager": "~=2.4",
-        "esp-idf-kconfig": "~=3.3.0"
+        "idf-component-manager": "~=2.4.8",
+        "esp-idf-kconfig": "~=3.6.0"
     }
 
     if sys_platform.system() == "Darwin" and "arm" in sys_platform.machine().lower():
