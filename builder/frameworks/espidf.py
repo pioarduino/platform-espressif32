b4a249de30aff7a8918cd653f2698782efdda507
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2026-09-25 11:48:34 +0200

    Disable src_filter in HybridCompile IDF Lib builder run (#547)

--- builder/frameworks/espidf.py
+++ builder/frameworks/espidf.py
@@ -1008,6 +1008,7 @@ if flag_custom_sdkonfig == True and "arduino" in env.subst("$PIOFRAMEWORK") and 
         BUILD_FLAGS="",
         BUILD_UNFLAGS="",
         LINKFLAGS="",
+        SRC_FILTER="-<*>",
         PIOFRAMEWORK="arduino",
         ARDUINO_LIB_COMPILE_FLAG="Build",
     )
@@ -2790,7 +2791,7 @@ if not os.path.isdir(PROJECT_SRC_DIR):
     )
     env.Exit(1)
 
-if env.subst("$SRC_FILTER"):
+if env.subst("$SRC_FILTER") and not flag_custom_sdkonfig:
     print(
         (
             "Warning: the 'src_filter' option cannot be used with ESP-IDF. Select source "
